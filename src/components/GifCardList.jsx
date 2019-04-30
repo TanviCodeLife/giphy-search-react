435b6ee847adb2b8a7c6b2c45bbb072ee5d723de
tanvi <tanvi.garg23@gmail.com>
2019-04-29 23:37:11 -0700

    correct styles for gif cards

--- src/components/GifCardList.jsx
+++ src/components/GifCardList.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import GifCard from "./GifCard";
 
+
+const GifWrapper = styled.div`
+ margin: 6%;
+ margin-left: 6%;
+ margin-right: 6%;
+
+  &::after{
+    content: "";
+    display: table;
+    clear: both;
+  }
+`;
+
 const GifCardList = () => {
   return (
-    <div>
+    <GifWrapper>
       <GifCard />
       <GifCard />
       <GifCard />
-    </div>
+    </GifWrapper>
   );
 };
 
