b9585f02f56d3ddfbcf41c3081084a0734d59a8a
tanvi <tanvi.garg23@gmail.com>
2019-04-29 23:01:10 -0700

    setup index.js entry point, dir structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { HashRouter } from "react-router-dom";
+import { AppContainer } from "react-hot-loader";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import App from "./components/App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const render = Component => {
+    ReactDOM.render(
+      <AppContainer>
+          <HashRouter>
+            <Component />
+          </HashRouter>
+      </AppContainer>,
+      document.getElementById("root")
+    );
+  };
+
+
+  render(App);
+
+  if (module.hot) {
+    module.hot.accept("./components/App", () => {
+      render(App);
+    });
+  }
+
 serviceWorker.unregister();
